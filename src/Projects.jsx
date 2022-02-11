7bc36e72e384f5165022ca5c02ac3618b1f17f86
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-11 13:38:48 +0100

    adding axios to my project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,17 +1,36 @@
-import React from "react";
-import { Container } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Container, Grid } from "semantic-ui-react";
+import axios from "axios";
 
-const Projects = () => {
+class Projects extends Component {
+  state = {
+    projects: [],
+  };
+
+  componentDidMount() {
+    axios.get("./data/projects.json").then((response) => {
+      this.setState({ projects: response.data });
+    });
+  }
+
+  render() {
+    const { projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+
     return (
-        <Container>
-            <h1 id="projects-header">My Projects</h1>
-            <p>
-                Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-                copporti fuga saepe distinctio ipsum? Et quos haram excepturi dolorum
-                molestias?
-            </p>
-        </Container>
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
     );
-};
+  }
+}
 
 export default Projects;
