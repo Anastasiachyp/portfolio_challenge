999b3b7b8bb0c4213b53b0704cb90d6eebeb646d
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-18 15:10:53 +0100

    give some style to my portfolio

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -3,13 +3,13 @@ import { Card, Image } from "semantic-ui-react";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div>
-      <Image alt={`${project.name} image`} src={project.image} wrapped ui={false} />
+    <Card>
+      <Image src={project.image} wrapped className="image"  />
       <div>
         <h3 className="header">{project.name}</h3>
         <p className="description">{project.description}</p>
       </div>
-    </div>
+    </Card>
   );
 };
 
